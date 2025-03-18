8ee98fc6002895118ea44773d02c8ad43cddff29
DivyamK774 <DivyamK190@stemcollegiate.ca>
2025-03-17 18:38:41 -0600

    Update and rename 1_ README.cs to 1_ README.md

--- 1_ README.cs
+++
@@ -1,2 +0,0 @@
-# My First Microgame
-//My microgame is a 3D go-karting game with many different custom features, like jump ramps all around the map, many speed boosts on the track, and a custom map to add complexity.
